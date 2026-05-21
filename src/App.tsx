2161032826ef1d8367f0a21811f0e0fc99459316
KV Group <kvgroup004@gmail.com>
2026-05-21 17:32:10 -0400

    refactor: normalize quote styles in App.tsx
    Update single quotes to double quotes throughout the router configuration for consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from './components/ui/sonner';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner";
 
-import Login from './routes/login';
-import AdminLayout from './routes/admin/_layout';
-import AdminIndex from './routes/admin/index';
+import Login from "./routes/login";
+import AdminLayout from "./routes/admin/_layout";
+import AdminIndex from "./routes/admin/index";
 
 // Módulo de clientes
-import ClientesIndex from './routes/admin/clientes/index';
-import ClienteNovo from './routes/admin/clientes/novo';
-import ClienteEdit from './routes/admin/clientes/$id';
+import ClientesIndex from "./routes/admin/clientes/index";
+import ClienteNovo from "./routes/admin/clientes/novo";
+import ClienteEdit from "./routes/admin/clientes/$id";
 
 // Módulos Auxiliares
-import FinanceiroIndex from './routes/admin/financeiro/index';
-import OrcamentosIndex from './routes/admin/orcamentos/index';
-import OrcamentoNovo from './routes/admin/orcamentos/novo';
-import Configuracoes from './routes/admin/configuracoes';
+import FinanceiroIndex from "./routes/admin/financeiro/index";
+import OrcamentosIndex from "./routes/admin/orcamentos/index";
+import OrcamentoNovo from "./routes/admin/orcamentos/novo";
+import Configuracoes from "./routes/admin/configuracoes";
 
 // Módulo de Dashboards
-import DashboardsIndex from './routes/admin/dashboards/index';
-import DashboardNovo from './routes/admin/dashboards/novo';
-import DashboardEditor from './routes/admin/dashboards/$id/editor';
+import DashboardsIndex from "./routes/admin/dashboards/index";
+import DashboardNovo from "./routes/admin/dashboards/novo";
+import DashboardEditor from "./routes/admin/dashboards/$id/editor";
 
 // Módulo de Funis
-import FunisIndex from './routes/admin/funis/index';
-import FunisNovo from './routes/admin/funis/novo';
-import FunisCanvas from './routes/admin/funis/$id/canvas';
+import FunisIndex from "./routes/admin/funis/index";
+import FunisNovo from "./routes/admin/funis/novo";
+import FunisCanvas from "./routes/admin/funis/$id/canvas";
 
 // Integrações
-import MetaConnectPage from './routes/admin/meta-connect';
+import MetaConnectPage from "./routes/admin/meta-connect";
+import InstagramPage from "./routes/admin/instagram/index";
 
 // Módulos Públicos Externos
-import OrcamentoPublico from './routes/orcamento/$token';
-import DashboardPublico from './routes/dashboard/$slug';
-import DashboardLancamento from './routes/dashboard/$slug/$lancamento';
+import OrcamentoPublico from "./routes/orcamento/$token";
+import DashboardPublico from "./routes/dashboard/$slug";
+import DashboardLancamento from "./routes/dashboard/$slug/$lancamento";
 
 // Páginas Institucionais Públicas
-import Privacidade from './routes/privacidade';
-import Termos from './routes/termos';
-import DeletarDados from './routes/deletar-dados';
+import Privacidade from "./routes/privacidade";
+import Termos from "./routes/termos";
+import DeletarDados from "./routes/deletar-dados";
 
-import { queryClient } from './lib/queryClient';
+import { queryClient } from "./lib/queryClient";
 
 export default function App() {
   return (
@@ -54,41 +55,44 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/admin" replace />} />
           <Route path="/login" element={<Login />} />
-          
+
           {/* Rotas Públicas */}
           <Route path="/privacidade" element={<Privacidade />} />
           <Route path="/termos" element={<Termos />} />
           <Route path="/deletar-dados" element={<DeletarDados />} />
           <Route path="/orcamento/:token" element={<OrcamentoPublico />} />
           <Route path="/dashboard/:slug" element={<DashboardPublico />} />
-          <Route path="/dashboard/:slug/:lancamento" element={<DashboardLancamento />} />
+          <Route
+            path="/dashboard/:slug/:lancamento"
+            element={<DashboardLancamento />}
+          />
 
           {/* Rotas Privadas (Admin) */}
           <Route path="/admin" element={<AdminLayout />}>
-             <Route index element={<AdminIndex />} />
-             
-             {/* Sub-rotas Clientes */}
-             <Route path="clientes" element={<ClientesIndex />} />
-             <Route path="clientes/novo" element={<ClienteNovo />} />
-             <Route path="clientes/:id" element={<ClienteEdit />} />
+            <Route index element={<AdminIndex />} />
 
-             {/* Sub-rotas Dashboards */}
-             <Route path="dashboards" element={<DashboardsIndex />} />
-             <Route path="dashboards/novo" element={<DashboardNovo />} />
-             <Route path="dashboards/:id/editor" element={<DashboardEditor />} />
+            {/* Sub-rotas Clientes */}
+            <Route path="clientes" element={<ClientesIndex />} />
+            <Route path="clientes/novo" element={<ClienteNovo />} />
+            <Route path="clientes/:id" element={<ClienteEdit />} />
 
-             {/* Sub-rotas Funis */}
-             <Route path="funis" element={<FunisIndex />} />
-             <Route path="funis/novo" element={<FunisNovo />} />
-             <Route path="funis/:id/canvas" element={<FunisCanvas />} />
+            {/* Sub-rotas Dashboards */}
+            <Route path="dashboards" element={<DashboardsIndex />} />
+            <Route path="dashboards/novo" element={<DashboardNovo />} />
+            <Route path="dashboards/:id/editor" element={<DashboardEditor />} />
 
-             {/* Sub-rotas Auxiliares */}
-             <Route path="financeiro" element={<FinanceiroIndex />} />
-             <Route path="orcamentos" element={<OrcamentosIndex />} />
-             <Route path="orcamentos/novo" element={<OrcamentoNovo />} />
-             <Route path="configuracoes" element={<Configuracoes />} />
-             <Route path="meta-connect" element={<MetaConnectPage />} />
-             
+            {/* Sub-rotas Funis */}
+            <Route path="funis" element={<FunisIndex />} />
+            <Route path="funis/novo" element={<FunisNovo />} />
+            <Route path="funis/:id/canvas" element={<FunisCanvas />} />
+
+            {/* Sub-rotas Auxiliares */}
+            <Route path="financeiro" element={<FinanceiroIndex />} />
+            <Route path="orcamentos" element={<OrcamentosIndex />} />
+            <Route path="orcamentos/novo" element={<OrcamentoNovo />} />
+            <Route path="instagram" element={<InstagramPage />} />
+            <Route path="configuracoes" element={<Configuracoes />} />
+            <Route path="meta-connect" element={<MetaConnectPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
